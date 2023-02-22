200a7b36179492ee496ad22989ed44043c1dec13
Crisb2022 <cacaceresb2@uce.edu.ec>
2023-02-22 12:22:51 -0500

    Commit 2

--- src/main/java/com/distribuida/db/Book.java
+++ src/main/java/com/distribuida/db/Book.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collation = "Book")
 public class Book {
-
     @Id
     private Integer id;
     private String isbn;
